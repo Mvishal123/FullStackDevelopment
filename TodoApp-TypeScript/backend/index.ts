e07b84b066bdbdbf01cd43def3cecf4c8b132048
Vishal-m <vishalmahesh.study@gmail.com>
2023-08-23 20:55:07 +0530

    moved JS to TS -partial

---
+++ TodoApp-TypeScript/backend/index.ts
@@ -0,0 +1,22 @@
+import express from "express"
+const app = express();
+import mongoose from "mongoose";
+const port = 3000;
+import authRoutes from "./routes/auth"
+import todoRoutes from "./routes/todo"
+import cors from "cors";
+
+app.use(cors());
+app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/todo", todoRoutes);
+
+mongoose.connect(
+    "mongodb+srv://vishalmaheshab:Roritomongo@cluster0.xanf5p6.mongodb.net/todos"
+);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
