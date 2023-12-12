c30951278686e492d3b90965796fb38e83fc0cb5
Vishal-m <vishalmahesh.study@gmail.com>
2023-12-12 17:40:46 +0530

    trpc server done

--- trpc/server/index.ts
+++ trpc/server/index.ts
@@ -1,8 +1,31 @@
-import { router } from './trpc';
- 
-const appRouter = router({
-  // ...
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
+import { publicProcedure, router } from "./trpc";
+import { z } from "zod";
+
+const todoType = z.object({
+  title: z.string(),
+  description: z.string(),
 });
- 
+
+const appRouter = router({
+  createTodo: publicProcedure.input(todoType).mutation((opts) => {
+    const title = opts.input.title;
+    const description = opts.input.description;
+
+    // db stuff
+
+    //response
+    return {
+      status: "success",
+      id: "1",
+    };
+  }),
+});
+
+const server = createHTTPServer({
+  router: appRouter,
+});
+
+server.listen(3000);
 
 export type AppRouter = typeof appRouter;
